5e106c4c0bcdf2928714c3dd62f88c5981e46860
Leandro0422 <leandro2003herrerafiayo@gmail.com>
2025-02-25 15:28:44 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products/{name}', function ($name) {
+    echo "El producto es $name";
+
+});
+
+Route::get('/products/{name}/{categoria?}', function ($name,$categoria=null) {
+    
+    if($categoria ==null){
+        echo "El producto ES: $name";
+    }else{
+            echo "El producto es $name, y su categoria es $categoria";
+        }
+    }
+
+);
+
