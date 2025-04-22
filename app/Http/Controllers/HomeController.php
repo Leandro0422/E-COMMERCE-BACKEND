83a9d81e186bcb84911d396ec0b8af0c2319daa7
Leandro0422 <leandro2003herrerafiayo@gmail.com>
2025-04-22 14:46:00 -0500

    configuración login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke(){
-        return view ('welcome');
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function welcome(){
+        return view("welcome");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', HomeController:: class);
 
 Route::prefix('/products')->controller(ProductController::class)->group(function(){
 
@@ -13,3 +12,9 @@ Route::prefix('/products')->controller(ProductController::class)->group(function
     Route::get('/show', 'show');
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController:: class, 'welcome']);
